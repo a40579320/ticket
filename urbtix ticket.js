99a15c043166e8f572dff07b3925d6094f7f4877
a40579320 <81283134+a40579320@users.noreply.github.com>
2022-10-11 10:50:50 +0800

    Add files via upload

---
+++ urbtix ticket.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         urbtix ticket
+// @namespace    http://tampermonkey.net/
+// @version      1.1
+// @description  try to take over the world!
+// @author       Oscar Leung
+// @match        http://msg.urbtix.hk/
+// @match        http://busy.urbtix.hk/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=uncle7world.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    location.replace("http://urbtix.hk");// Your code here...
+})();
