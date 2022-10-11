b378d7655a0a4b40aaadfd3554f28135638f6352
a40579320 <81283134+a40579320@users.noreply.github.com>
2022-10-11 10:57:48 +0800

    Create cityline.js

---
+++ cityline.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         cityline
+// @namespace    http://tampermonkey.net/
+// @version      1.1
+// @description  cityline
+// @author       Oscar Leung
+// @match        http://msg.cityline.com/busy.html
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=uncle7world.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    location.replace("https://www.cityline.com/Events.html");// Your code here...
+})();
